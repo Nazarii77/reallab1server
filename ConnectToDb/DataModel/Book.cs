a8f222e3a8ecf01f7654f566b273b16040337fc7
Nazarii77 <boichyshyn@gmail.com>
2017-10-31 01:13:19 +0200

    adding ukrainian+ few bugs fixes

---
+++ ConnectToDb/DataModel/Book.cs
@@ -0,0 +1,19 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ConnectToDb.DataModel
+//{
+//    public class Book
+//    {
+//        public int BookID { get; set; }
+//        public string Title { get; set; }
+//        public string Author { get; set; }
+
+//        public virtual ICollection<Author> Authors { get; set; }
+
+//    }
+//}
+//// <connectionStrings><add name="LibraryContext.cs" connectionString="data source=.\MSSQLEXPRESS;initial catalog=LibraryDb;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework" providerName="System.Data.SqlClient" /></connectionStrings></configuration>
---
+++ Interface/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Interface.DataModel;
+
+namespace Interface
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+       
+        }
+    }
+}
